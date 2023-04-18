fdc972ce704f639f05305751a8f18397d14d06aa
KipoGo <vit-tan@mail.ru>
2023-04-18 13:13:00 +0300

    Refactoring

---
+++ Assets/Scripts/MonoProviders/CubeInitProvider.cs
@@ -0,0 +1,9 @@
+﻿using Voody.UniLeo;
+
+namespace MagicCubes.Cube
+{
+    public class CubeInitProvider : MonoProvider<CubeInitComponent>
+    {
+
+    }
+}
--- Assets/Scripts/MonoProviders/InitProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Voody.UniLeo;
-
-namespace MagicCubes.Cube
-{
-    public class InitProvider : MonoProvider<CubeInitComponent>
-    {
-
-    }
-}
