9f23227c32bdd55bfbc334d06b1a6ee1c046fccc
KipoGo <vit-tan@mail.ru>
2023-04-18 17:49:54 +0300

    Add configs

---
+++ Assets/Scripts/Config/Configurations.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MagicCubes.Config
+{
+    [CreateAssetMenu(fileName = "Configurations", menuName = "Config/Configurations")]
+    public class Configurations : ScriptableObject
+    {
+        [SerializeField] private LvlHolderConfig _lvlHolderConfig;
+    }
+}
---
+++ Assets/Scripts/Config/Levels/LvlHolderConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MagicCubes.Config
+{
+    [CreateAssetMenu(fileName = "LvlHolderConfig", menuName = "Config/LvlHolderConfig")]
+    public class LvlHolderConfig : ScriptableObject
+    {
+        [SerializeField] private Scene[] _scenes;
+    }
+}
