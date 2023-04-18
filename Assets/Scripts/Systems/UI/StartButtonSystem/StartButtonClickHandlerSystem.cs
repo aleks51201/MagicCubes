bdb781984eee9419e72a746d7b496ba53524fd74
KipoGo <vit-tan@mail.ru>
2023-04-18 17:50:53 +0300

    Add feature
    for register and handle callback

---
+++ Assets/Scripts/Systems/UI/StartButtonSystem/StartButtonClickHandlerSystem.cs
@@ -0,0 +1,27 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Ui;
+using UnityEngine.UIElements;
+
+namespace Systems.UI
+{
+    public class StartButtonClickHandlerSystem : IEcsRunSystem
+    {
+        private readonly EcsFilter<StartButtonClickEvent, StartButtonComponent> _clickFilter;
+        private readonly EcsFilter<StartMenuLevelChooseScreenComponent> _lvlChooseScreenFilter;
+
+
+        public void Run()
+        {
+            foreach (var index in _clickFilter)
+            {
+                foreach (var indexJ in _lvlChooseScreenFilter)
+                {
+                    ref var lvlScreen = ref _lvlChooseScreenFilter.Get1(indexJ).LvlChooseScreen;
+                    lvlScreen.style.display = DisplayStyle.Flex;
+                }
+                _clickFilter.Get2(index).Button.style.display = DisplayStyle.None;
+                _clickFilter.GetEntity(index).Del<StartButtonClickEvent>();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/UI/StartButtonSystem/StartButtonRegisterCallbackSystem.cs
@@ -0,0 +1,28 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Ui;
+using UnityEngine.UIElements;
+
+namespace Systems.UI
+{
+    public class StartButtonRegisterCallbackSystem : IEcsInitSystem
+    {
+        private readonly EcsFilter<StartButtonComponent> _startButtonFilter = null;
+
+
+        public void Init()
+        {
+            foreach(var index in _startButtonFilter)
+            {
+                _startButtonFilter.Get1(0).Button.RegisterCallback<ClickEvent>(OnButtonClick);
+            }
+        }
+
+        private void OnButtonClick(ClickEvent clickEvent)
+        {
+            foreach(var index in _startButtonFilter)
+            {
+                _startButtonFilter.GetEntity(index).Get<StartButtonClickEvent>();
+            }
+        }
+    }
+}
