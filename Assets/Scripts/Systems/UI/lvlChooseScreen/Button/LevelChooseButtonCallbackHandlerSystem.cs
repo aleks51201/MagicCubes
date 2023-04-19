942f7c3160baf1ffda867f7da5b8dc62252a4714
KipoGo <vit-tan@mail.ru>
2023-04-19 17:38:22 +0300

    Add system
    for register event

---
+++ Assets/Scripts/Systems/UI/lvlChooseScreen/Button/LevelChooseButtonCallbackHandlerSystem.cs
@@ -0,0 +1,27 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components.Ui;
+using MagicCubes.Events.Ui;
+using UnityEngine.SceneManagement;
+
+namespace MagicCubes.Systems.UI
+{
+    internal sealed class LevelChooseButtonCallbackHandlerSystem : IEcsRunSystem
+    {
+        private readonly EcsFilter<LvlChooseButtonClickEvent, LevelChooseBtnComponent, LevelElementComponent> _btnFilter = null;
+
+
+        public void Run()
+        {
+            foreach (var index in _btnFilter)
+            {
+                _btnFilter.GetEntity(index).Del<LvlChooseButtonClickEvent>();
+                if (!_btnFilter.Get2(index).ButtonStatusHolder.IsClicked)
+                {
+                    continue;
+                }
+                SceneManager.LoadScene(_btnFilter.Get3(index).SceneName);
+                _btnFilter.Get2(index).ButtonStatusHolder.StatusReset();
+            }
+        }
+    }
+}
