58490a2c0e20c4b60307787a72e71e039ef8ab67
KipoGo <vit-tan@mail.ru>
2023-04-19 13:22:54 +0300

    Add component

---
+++ Assets/Scripts/Components/UI/LevelElementComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace MagicCubes.Components.Ui
+{
+    internal struct LevelElementComponent
+    {
+        internal VisualTreeAsset LvlElement;
+        internal string SceneName;
+    }
+}
