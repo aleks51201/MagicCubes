933f2094d8c6a1a351ffdd6f3e69b4a86d9d1829
KipoGo <vit-tan@mail.ru>
2023-04-19 17:37:46 +0300

    Add feature
    for tracking event

---
+++ Assets/Scripts/Components/UI/ButtonStatusHolder.cs
@@ -0,0 +1,25 @@
+﻿namespace MagicCubes.Components.Ui
+{
+    internal class ButtonStatusHolder
+    {
+        internal bool IsClicked { get; private set; }
+
+
+        public ButtonStatusHolder()
+        {
+            IsClicked = false;
+
+        }
+
+
+        internal void OnClicked()
+        {
+            IsClicked = true;
+        }
+
+        internal void StatusReset()
+        {
+            IsClicked = false;
+        }
+    }
+}
