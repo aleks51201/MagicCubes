0aa4ad31d9b9d3d2ae8b67a7b25ca64f584940d1
KipoGo <vit-tan@mail.ru>
2023-04-19 16:05:15 +0300

    Add system
    for validate scene name

---
+++ Assets/Scripts/Systems/SceneConfigValidatorSystem.cs
@@ -0,0 +1,25 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Config;
+using System;
+using UnityEngine.SceneManagement;
+
+namespace MagicCubes.Systems
+{
+    internal sealed class SceneConfigValidatorSystem : IEcsInitSystem
+    {
+        private readonly Configurations _configurations = null;
+
+        public void Init()
+        {
+            foreach (string sceneName in _configurations.LvlHolderConfig.SceneNames)
+            {
+                var scene = SceneManager.GetSceneByName(sceneName);
+                if (scene.name != sceneName)
+                {
+                    throw new ArgumentException($"Scene with name = {sceneName} not exist");
+                }
+            }
+        }
+    }
+
+}
