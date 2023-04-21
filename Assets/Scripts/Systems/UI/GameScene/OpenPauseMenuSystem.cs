2f663b90170a90389bf522e7f6d0f1bcac302038
KipoGo <vit-tan@mail.ru>
2023-04-21 13:46:38 +0300

    Add system
    for open and close pause menu

---
+++ Assets/Scripts/Systems/UI/GameScene/OpenPauseMenuSystem.cs
@@ -0,0 +1,53 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components;
+using MagicCubes.Components.Ui;
+using UnityEngine.UIElements;
+
+namespace MagicCubes.Systems.UI.GameScene
+{
+    public sealed class OpenPauseMenuSystem : IEcsRunSystem
+    {
+        private readonly EcsFilter<InputComponent> _inputFilter = null;
+        private readonly EcsFilter<UIInitComponent> _uiFilter = null;
+
+        private const string ButtonsUIHolder = "ButtonsUIHolder";
+
+        private bool _oneClick;
+
+        public void Run()
+        {
+            foreach (var index in _inputFilter)
+            {
+                OnEscapeButtonDown(index);
+                OnEscapeButtonUp(index);
+            }
+        }
+
+        private void OnEscapeButtonDown(int index)
+        {
+            if (!_inputFilter.Get1(index).EscapeButtonDown || _oneClick)
+            {
+                return;
+            }
+            var displayStyle = _uiFilter.Get1(index).UIDocument.rootVisualElement.Q(ButtonsUIHolder).style.display;
+            if (displayStyle == DisplayStyle.Flex)
+            {
+                _uiFilter.Get1(index).UIDocument.rootVisualElement.Q(ButtonsUIHolder).style.display = DisplayStyle.None;
+            }
+            else
+            {
+                _uiFilter.Get1(index).UIDocument.rootVisualElement.Q(ButtonsUIHolder).style.display = DisplayStyle.Flex;
+            }
+            _oneClick = true;
+        }
+
+        private void OnEscapeButtonUp(int index)
+        {
+            if (!_inputFilter.Get1(index).EscapeButtonUp || !_oneClick)
+            {
+                return;
+            }
+            _oneClick = false;
+        }
+    }
+}
