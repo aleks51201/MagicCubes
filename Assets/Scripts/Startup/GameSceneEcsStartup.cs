90415c92f611044746221cdc642af9efd0e6cd75
KipoGo <vit-tan@mail.ru>
2023-04-21 12:24:52 +0300

    Add class
    for start ecs in game scene

---
+++ Assets/Scripts/Startup/GameSceneEcsStartup.cs
@@ -0,0 +1,28 @@
+﻿using MagicCubes.Systems;
+using MagicCubes.Systems.UI.GameScene;
+
+namespace MagicCubes
+{
+    public class GameSceneEcsStartup : EcsStartupBase
+    {
+        private protected override void AddInjections()
+        {
+            base.AddInjections();
+        }
+
+        private protected override void AddOneFrames()
+        {
+        }
+
+        private protected override void AddSystems()
+        {
+            _systems
+                .Add(new InitGameSceneUISystem())
+                .Add(new CreateEntityForCubeViewSystem())
+                .Add(new ScoringSystem())
+                .Add(new RotationSystem())
+                .Add(new WinRotationCheckSystem())
+                .Add(new WinSystem());
+        }
+    }
+}
