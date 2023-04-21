89fdc052f828b87ff0bfcea05bfc4e8858e0bf7e
KipoGo <vit-tan@mail.ru>
2023-04-21 18:02:30 +0300

    Add system
    for handle back to menu button

---
+++ Assets/Scripts/Systems/UI/GameScene/PauseMenu/BackToMenuButton/BackToMenuButtonCallbackHandlerSystem.cs
@@ -0,0 +1,29 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components.Ui;
+using MagicCubes.Events.Ui;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace MagicCubes.Systems.UI
+{
+    internal sealed class BackToMenuButtonCallbackHandlerSystem : IEcsRunSystem
+    {
+        private readonly EcsFilter<BackButtonToMenuButtonClickEvent, BackToMenuButtonComponent> _btnFilter = null;
+
+        private const string MenuScene = "MenuScene";
+
+        public void Run()
+        {
+            foreach (var index in _btnFilter)
+            {
+                _btnFilter.GetEntity(index).Del<BackButtonToMenuButtonClickEvent>();
+                if (!_btnFilter.Get2(index).ButtonStatusHolder.IsClicked)
+                {
+                    continue;
+                }
+                SceneManager.LoadScene(MenuScene);
+                _btnFilter.Get2(index).ButtonStatusHolder.StatusReset();
+            }
+        }
+    }
+}
