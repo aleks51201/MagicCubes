448a64bf9030d6f5050f0e6d6d01a5fc17864a1d
KipoGo <vit-tan@mail.ru>
2023-04-21 12:25:36 +0300

    Add system
    for scoring

---
+++ Assets/Scripts/Systems/UI/GameScene/ScoringSystem.cs
@@ -0,0 +1,25 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components.Ui;
+using MagicCubes.Events;
+using UnityEngine.UIElements;
+
+namespace MagicCubes.Systems.UI.GameScene
+{
+    public sealed class ScoringSystem : IEcsRunSystem
+    {
+        private readonly EcsFilter<RotateEvent> _rotateFilter = null;
+        private readonly EcsFilter<UIInitComponent> _uiFilter = null;
+
+        private const string RotationsCount = "RotationsCount";
+
+        private int _rotateCount = 0;
+
+        public void Run()
+        {
+            foreach (var index in _rotateFilter)
+            {
+                _uiFilter.Get1(index).UIDocument.rootVisualElement.Q<Label>(RotationsCount).text = $"Rotations: {_rotateCount + 1}";
+            }
+        }
+    }
+}
