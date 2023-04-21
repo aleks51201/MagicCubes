1cef5b5aface580bc99a846ab1a52b024e692a47
KipoGo <vit-tan@mail.ru>
2023-04-21 12:25:08 +0300

    Add system
    for init game scene

---
+++ Assets/Scripts/Systems/UI/GameScene/InitGameSceneUISystem.cs
@@ -0,0 +1,23 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components.Ui;
+using MagicCubes.Events;
+using UnityEngine.UIElements;
+
+namespace MagicCubes.Systems.UI.GameScene
+{
+    public sealed class InitGameSceneUISystem : IEcsInitSystem
+    {
+        private readonly EcsFilter<UIInitComponent> _uiFilter = null;
+
+        private const string GameUI = "GameUI";
+
+        public void Init()
+        {
+            foreach (var index in _uiFilter)
+            {
+                VisualElement visualElement = _uiFilter.Get1(index).UIDocument.rootVisualElement;
+                visualElement.Q(GameUI).style.display = DisplayStyle.Flex;
+            }
+        }
+    }
+}
