8e96ed862fc84070db91546112fdd2498311c785
KipoGo <vit-tan@mail.ru>
2023-04-21 13:46:16 +0300

    Add component
    for input

---
+++ Assets/Scripts/Components/InputComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace MagicCubes.Components
+{
+    internal struct InputComponent
+    {
+        internal Input Input;
+        internal bool EscapeButtonDown;
+        internal bool EscapeButtonUp;
+    }
+}
