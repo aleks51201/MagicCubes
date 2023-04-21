1349af455f42459e4984f36fb3dbba9cb6ef2f1f
KipoGo <vit-tan@mail.ru>
2023-04-21 18:01:03 +0300

    Add component

---
+++ Assets/Scripts/Components/UI/GameScene/PauseMenu/ResetButtonComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UIElements;
+
+namespace MagicCubes.Components.Ui
+{
+    internal struct ResetButtonComponent
+    {
+        internal Button Button;
+        private ButtonStatusHolder _buttonStatusHolder;
+        internal ButtonStatusHolder ButtonStatusHolder;
+/*        internal ButtonStatusHolder ButtonStatusHolder 
+        {
+            get
+            {
+                if(_buttonStatusHolder == null)
+                {
+                    _buttonStatusHolder = new();
+                }
+                return _buttonStatusHolder;
+            }
+        }
+*/    }
+}
