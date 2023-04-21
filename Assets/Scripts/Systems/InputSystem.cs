48caae793e315a08de171b62835eec87f557f691
KipoGo <vit-tan@mail.ru>
2023-04-21 13:45:47 +0300

    Add system
    for input

---
+++ Assets/Scripts/Systems/InputSystem.cs
@@ -0,0 +1,26 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components;
+using UnityEngine;
+
+namespace MagicCubes.Systems.UI.GameScene
+{
+    public sealed class InputSystem :IEcsInitSystem, IEcsRunSystem
+    {
+        private readonly EcsWorld _world = null;
+        private readonly EcsFilter<InputComponent> _inputFilter = null;
+
+        public void Init()
+        {
+            _world.NewEntity().Get<InputComponent>();
+        }
+
+        public void Run()
+        {
+            foreach(var index in _inputFilter)
+            {
+                _inputFilter.Get1(index).EscapeButtonDown = Input.GetButtonDown("Escape");
+                _inputFilter.Get1(index).EscapeButtonUp = Input.GetButtonUp("Escape");
+            }
+        }
+    }
+}
