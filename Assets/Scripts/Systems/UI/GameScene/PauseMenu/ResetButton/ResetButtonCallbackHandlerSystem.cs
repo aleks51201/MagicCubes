59c220e67724b14727e638b347fc7812b32b49f9
KipoGo <vit-tan@mail.ru>
2023-04-21 18:03:58 +0300

    Add system
    for register callback reset button

---
+++ Assets/Scripts/Systems/UI/GameScene/PauseMenu/ResetButton/ResetButtonCallbackHandlerSystem.cs
@@ -0,0 +1,29 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components.Ui;
+using MagicCubes.Events.Ui;
+using UnityEngine.SceneManagement;
+
+namespace MagicCubes.Systems.UI
+{
+    internal sealed class ResetButtonCallbackHandlerSystem : IEcsRunSystem
+    {
+        private readonly EcsFilter<ResetButtonClickEvent, ResetButtonComponent> _btnFilter = null;
+
+
+        public void Run()
+        {
+            foreach (var index in _btnFilter)
+            {
+                _btnFilter.GetEntity(index).Del<ResumeButtonClickEvent>();
+                if (!_btnFilter.Get2(index).ButtonStatusHolder.IsClicked)
+                {
+                    continue;
+                }
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.UnloadSceneAsync(scene.name);
+                SceneManager.LoadScene(scene.name);
+                _btnFilter.Get2(index).ButtonStatusHolder.StatusReset();
+            }
+        }
+    }
+}
