d13731584eacee1711a0e44057d9953a884ccbd1
KipoGo <vit-tan@mail.ru>
2023-04-27 11:29:41 +0300

    Add system

---
+++ Assets/Scripts/Systems/UI/GameScene/WinMenu/NextLvlButton/NextLvlButtonCallbackHandlerSystem.cs
@@ -0,0 +1,47 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components.Ui;
+using MagicCubes.Config;
+using MagicCubes.Events.Ui;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MagicCubes.Systems.UI
+{
+    internal sealed class NextLvlButtonCallbackHandlerSystem : IEcsRunSystem
+    {
+        private readonly EcsWorld _world = null;
+        private readonly EcsFilter<NextLvlButtonToMenuButtonClickEvent, NextLvlButtonComponent> _btnFilter = null;
+        private readonly EcsFilter<CurrentLvlComponent> _currentFilter = null;
+        private readonly Configurations _configurations;
+
+        private const string MenuScene = "MenuScene";
+
+        public void Run()
+        {
+            foreach (var index in _btnFilter)
+            {
+                Debug.Log("NextLvlButtonCallbackHandlerSystem");
+                _btnFilter.GetEntity(index).Del<NextLvlButtonToMenuButtonClickEvent>();
+                if (!_btnFilter.Get2(index).ButtonStatusHolder.IsClicked)
+                {
+                    continue;
+                }
+                _btnFilter.Get2(index).ButtonStatusHolder.StatusReset();
+                _world.NewEntity().Get<ClosedWinMenuEvent>();
+                foreach(var i in _currentFilter)
+                {
+                    Debug.Log("NextLvlButtonCallbackHandlerSystem _currentFilter");
+                    int id = _currentFilter.Get1(i).Id + 1;
+                    if(id < _configurations.LvlHolderConfig.SceneNames.Length)
+                    {
+                        SceneManager.LoadScene(_configurations.LvlHolderConfig.SceneNames[id]);
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene(MenuScene);
+                    }
+                }
+            }
+        }
+    }
+}
