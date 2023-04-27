e34add1d71f01d60b75e422e64a01bc732ab95ee
KipoGo <vit-tan@mail.ru>
2023-04-27 11:28:51 +0300

    Update system

--- Assets/Scripts/Systems/UI/GameScene/PauseMenu/ResetButton/ResetRegisterCallBackSystem.cs
+++ Assets/Scripts/Systems/UI/GameScene/PauseMenu/ResetButton/ResetRegisterCallBackSystem.cs
@@ -8,19 +8,42 @@ namespace MagicCubes.Systems.UI
     public sealed class ResetRegisterCallBackSystem : IEcsRunSystem
     {
         private readonly EcsWorld _ecsWorld = null;
-        private readonly EcsFilter<OpenedPauseMenuEvent> _openedEventFilter = null;
         private readonly EcsFilter<UIInitComponent> _uiFilter = null;
         private readonly EcsFilter<ResetButtonComponent> _resetBtnFilter = null;
+        private readonly EcsFilter<OpenedPauseMenuEvent> _openedPauseMenuFilter = null;
+        private readonly EcsFilter<OpenedWinMenuEvent> _openedWinMenuFilter = null;
+        private readonly EcsFilter<ClosedPauseMenuEvent> _closedPauseMenuFilter = null;
+        private readonly EcsFilter<ClosedWinMenuEvent> _closedWinMenuFilter = null;
 
-        private const string ResetGame= "ResetGame";
+        private const string ResetGame = "ResetGame";
 
         public void Run()
         {
-            foreach(var index in _openedEventFilter)
+            foreach (var index in _openedPauseMenuFilter)
             {
-                foreach(var indexJ in _uiFilter)
+                Register(index);
+            }
+            foreach (var index in _openedWinMenuFilter)
+            {
+                Register(index);
+            }
+            foreach (var index in _closedPauseMenuFilter)
+            {
+                Unregister(index);
+            }
+            foreach (var index in _closedWinMenuFilter)
+            {
+                Unregister(index);
+            }
+        }
+
+        private void Register(int index)
+        {
+            foreach (var indexJ in _uiFilter)
+            {
+                var btns = _uiFilter.Get1(index).UIDocument.rootVisualElement.Query<Button>(ResetGame).ToList();
+                foreach (var btn in btns)
                 {
-                    Button btn = _uiFilter.Get1(index).UIDocument.rootVisualElement.Q<Button>(ResetGame);
                     var resetComponent = new ResetButtonComponent()
                     {
                         Button = btn,
@@ -30,6 +53,20 @@ namespace MagicCubes.Systems.UI
                     resetComponent.Button = btn;
                     btn.clicked += resetComponent.ButtonStatusHolder.OnClicked;
                     btn.clicked += OnClick;
+                }
+            }
+        }
+
+        private void Unregister(int index)
+        {
+            foreach (var indexJ in _uiFilter)
+            {
+                foreach (var i in _resetBtnFilter)
+                {
+                    Button btn = _resetBtnFilter.Get1(i).Button;
+                    var resetComponent = _resetBtnFilter.Get1(i);
+                    btn.clicked -= resetComponent.ButtonStatusHolder.OnClicked;
+                    btn.clicked -= OnClick;
                 }
             }
         }
