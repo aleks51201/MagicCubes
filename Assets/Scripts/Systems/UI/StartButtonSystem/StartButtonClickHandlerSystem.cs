f0343db2564eee061f0a951f7f68bff4a919be2d
KipoGo <vit-tan@mail.ru>
2023-05-02 12:40:38 +0300

    Fix bug

--- Assets/Scripts/Systems/UI/StartButtonSystem/StartButtonClickHandlerSystem.cs
+++ Assets/Scripts/Systems/UI/StartButtonSystem/StartButtonClickHandlerSystem.cs
@@ -10,8 +10,10 @@ namespace MagicCubes.Systems.UI
         private readonly EcsFilter<StartButtonComponent> _clickFilter;
         private readonly EcsFilter<StartButtonClickEvent> _clickEventFilter;
         private readonly EcsFilter<StartMenuLevelChooseScreenComponent> _lvlChooseScreenFilter;
+        private readonly EcsFilter<UIInitComponent> _uiFilter;
 
 
+        private const string StartScreen = "StartScreen";
         public void Run()
         {
             foreach (var indexi in _clickEventFilter)
@@ -22,7 +24,11 @@ namespace MagicCubes.Systems.UI
                     {
                         continue;
                     }
-                    _clickFilter.Get1(index).Button.style.display = DisplayStyle.None;
+                    foreach(var i in _uiFilter)
+                    {
+                        _uiFilter.Get1(i).UIDocument.rootVisualElement.Q(StartScreen).style.display = DisplayStyle.None;
+                    }
+                    //_clickFilter.Get1(index).Button.style.display = DisplayStyle.None;
                     _clickFilter.Get1(index).ButtonStatusHolder.StatusReset();
                     _lvlChooseScreenFilter.GetEntity(index).Get<StartButtonClickedEvent>();
                 }
