4eacfcc35fb31e917c9fe5dbe3ee84e2d2d90499
KipoGo <vit-tan@mail.ru>
2023-05-02 12:38:21 +0300

    Add feature
    for sub and unsub button

---
+++ Assets/Scripts/Components/UI/MenuScene/ButtonEventProceeder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+namespace MagicCubes.Components.Ui
+{
+    internal class ButtonEventProceeder
+    {
+        private Button _button;
+        private List<Action> _actions;
+
+
+        public ButtonEventProceeder(Button button)
+        {
+            _button = button;
+            _actions = new();
+        }
+
+
+        internal void Subscribe(Action action)
+        {
+            _button.clicked += action;
+            _actions.Add(action);
+        }
+
+        internal void Unsubscribe(Action action)
+        {
+            if (!_actions.Exists(x => x == action))
+            {
+                return;
+            }
+            _button.clicked -= action;
+            _actions.Remove(action);
+        }
+
+        internal void UnsubscribeAll()
+        {
+            for(var i = _actions.Count - 1; i>= 0; i--)
+            {
+                Unsubscribe(_actions[i]);
+            }
+        }
+    }
+}
