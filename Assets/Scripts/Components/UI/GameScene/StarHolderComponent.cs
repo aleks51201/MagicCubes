5bcac45dd92279b5f4382ede66771c5501369042
KipoGo <vit-tan@mail.ru>
2023-05-28 12:38:11 +0300

    Add component
    for save count star

---
+++ Assets/Scripts/Components/UI/GameScene/StarHolderComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MagicCubes.Components.Ui
+{
+    public struct StarHolderComponent
+    {
+        private int _starCount;
+        public int StarCount
+        {
+            get
+            {
+                return _starCount;
+            }
+            set
+            {
+                if (value > 3 || value < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"Value can be '0 <= value <= 3', current value = {value}");
+                }
+                _starCount = value;
+            }
+        }
+    }
+}
