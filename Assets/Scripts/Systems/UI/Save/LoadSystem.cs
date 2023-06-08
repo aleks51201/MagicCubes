b83663a6811043f53cb4c273d337c83182e4037b
KipoGo <vit-tan@mail.ru>
2023-06-08 15:46:52 +0300

    Add system
    for load saves

---
+++ Assets/Scripts/Systems/UI/Save/LoadSystem.cs
@@ -0,0 +1,29 @@
+﻿using Leopotam.Ecs;
+using MagicCubes.Components.Ui.Save;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace MagicCubes.Systems.UI.Save
+{
+    public class LoadSystem: IEcsInitSystem
+    {
+        private readonly EcsWorld _world;
+
+        private const string KEY = "GameSaves";
+
+
+        public void Init()
+        {
+            if (PlayerPrefs.HasKey(KEY))
+            {
+                string str = PlayerPrefs.GetString(KEY);
+                _world.NewEntity().Get<SavesComponent>() = Deserialize(str);
+            }
+        }
+
+        private SavesComponent Deserialize(string str)
+        {
+            return JsonConvert.DeserializeObject<SavesComponent>(str);
+        }
+    }
+}
