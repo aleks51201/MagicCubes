b2de3f4b7394f4fea380c63d0b3a09a1aee77e56
KipoGo <vit-tan@mail.ru>
2023-06-08 16:01:53 +0300

    Add tools
    for reset saves

---
+++ Assets/Scripts/Editor/EditorTools/ResetSavesBtn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MagicCubes.EditTool
+{
+    public class ResetSavesBtn : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
