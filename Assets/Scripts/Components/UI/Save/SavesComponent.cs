25bdd4fb253319926204812a993a015aecd04aaa
KipoGo <vit-tan@mail.ru>
2023-06-08 15:45:07 +0300

    Add component

---
+++ Assets/Scripts/Components/UI/Save/SavesComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MagicCubes.Components.Ui.Save
+{
+    public struct SavesComponent
+    {
+        public Level[] Levls;
+    }
+
+    [Serializable]
+    public class Level
+    {
+        public string SceneName;
+        public int Stars;
+    }
+}
