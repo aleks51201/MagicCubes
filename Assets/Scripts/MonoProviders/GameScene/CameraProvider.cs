06437fc28a1ac69ab4835c81bb3753e1706e7459
KipoGo <vit-tan@mail.ru>
2023-06-11 20:48:34 +0300

    Add provider
    camera component for monoBeh

---
+++ Assets/Scripts/MonoProviders/GameScene/CameraProvider.cs
@@ -0,0 +1,10 @@
+﻿using MagicCubes.Components.GameScene;
+using Voody.UniLeo;
+
+namespace MagicCubes.MonoProviders.Cube
+{
+    public class CameraProvider : MonoProvider<CameraComponent>
+    {
+
+    }
+}
