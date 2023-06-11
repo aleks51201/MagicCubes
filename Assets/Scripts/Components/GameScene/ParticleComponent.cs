fed076f216c58e1060d77071c8ca449245720df2
KipoGo <vit-tan@mail.ru>
2023-06-11 20:47:44 +0300

    Add component
    for cashe activated particle system

---
+++ Assets/Scripts/Components/GameScene/ParticleComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagicCubes.Components.GameScene
+{
+    internal struct ParticleComponent
+    {
+        internal List<ParticleSystem> CurrentParticleSystem;
+    }
+}
