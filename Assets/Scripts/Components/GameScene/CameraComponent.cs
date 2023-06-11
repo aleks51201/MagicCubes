dfd47c7b8df88bc59c3ffe1394a62485698d2c6b
KipoGo <vit-tan@mail.ru>
2023-06-11 20:46:48 +0300

    Add component
    for camera cashe

---
+++ Assets/Scripts/Components/GameScene/CameraComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace MagicCubes.Components.GameScene
+{
+    [Serializable]
+    public struct CameraComponent
+    {
+        public Camera Camera;
+        //public LayerMask LayerMask;
+    }
+}
